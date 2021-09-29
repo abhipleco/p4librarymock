e6f6f95b89873dbb13461af1e48ebdf3dae5d5bb
abhipleco <abhijit.vichare@plecosystems.com>
2021-09-30 01:48:56 +0530

    P4MOCK library

---
+++ p4library/workspacediscovery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EA.WorkspaceManager.p4library
+{
+    public class workspacediscovery
+    {
+        public static List<string> Main(string[] args)
+        {
+            List<string> workspacelist = new List<string>();
+            workspacelist.Add("abetz_ORL2-ABETZ-R_7915");
+            workspacelist.Add("abetz_ORL2-ABETZ-R_8325");
+            workspacelist.Add("abetz_ORL2-ABETZ-R_MODIv2_dev");
+            workspacelist.Add("abetz_ORL2-ABETZ-R_Service911");
+            workspacelist.Add("abetz_ORL2-ABETZ-R_Webapps");
+            workspacelist.Add("AdobeRequests");
+            workspacelist.Add("afrangulea_AFJAASVm_4086");
+            workspacelist.Add("afrangulea_AFJAASVm_4966");
+            workspacelist.Add("afrangulea_AFJAASVm_6589");
+            workspacelist.Add("afrangulea_AFJAASVm_6879");
+            workspacelist.Add("afrangulea_AFJAASVm_newdev");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_1014");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_1309");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_1423");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_1935");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_2003");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_7354");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_8833");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_9147");
+            workspacelist.Add("afrangulea_Jukb7d1e81Vm_9179");
+            workspacelist.Add("afrangulea_PerfOne");
+            workspacelist.Add("afrangulea_RO-3046820-W8_HAL35_Dev_2927");
+            workspacelist.Add("afrangulea_RO-36505551_5247");
+            workspacelist.Add("afrangulea_RO-36505551_5771");
+            workspacelist.Add("ahung_P4One");
+            workspacelist.Add("avichare_Per774ce48Vm_p4one_pleco_6314");
+
+            return workspacelist;
+
+        }
+
+
+
+    }
+}
