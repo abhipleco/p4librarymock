b00a711597b92a9af387ff23139211f424a68bc3
abhipleco <abhijit.vichare@plecosystems.com>
2021-09-30 22:34:52 +0530

    check p4library

---
+++ p4librarymock/WorkpacesDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EA.WorkspaceManager.p4library
+{
+    public class WorkpacesDetails
+    {
+        public static List<Workspace> getWorkpacesDetails(string p4instancename)
+        {
+            //Accept p4instancename as argument 
+
+            var workspacelist = new List<Workspace>
+            {
+                new Workspace { Instancename = "gdas.p4one.ea.com:1666", Wsname = "abetz_ORL2-ABETZ-R_7915", Owner = "abetz", Time = "2021/05/26 15:51:57",Filelock = "N" }
+
+
+             };
+
+            //return Mock data for workspace
+            return workspacelist;
+        }
+    }
+}
---
+++ p4librarymock/Workspace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EA.WorkspaceManager.p4library
+{
+    public class Workspace
+    {
+        public string Instancename { get; set; }
+        public string Wsname { get; set; }
+        public string Owner { get; set; }
+        public string Time { get; set; }
+        public string Filelock { get; set; }
+
+
+    }
+}
